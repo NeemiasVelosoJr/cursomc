91bb1eb37904581f1bc47c3de201e5f136009878
Neemias Veloso Jr <neemiastvjr@gmail.com>
2021-01-12 16:24:41 -0300

    Criando repositoty e service para categoria

---
+++ src/main/java/com/neemiasvelosojr/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.neemiasvelosojr.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.neemiasvelosojr.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+	
+}
